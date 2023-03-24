8bb52e472e157bc91df056c939e62aa27eb6485c
ostrev <npostrev@gmail.com>
2023-03-24 15:20:29 +0200

    add

--- JS Front-End/10_Exercise DOM and Events/More Exercises/12. Encode-and-Decode-Messages/app.js
+++ JS Front-End/10_Exercise DOM and Events/More Exercises/12. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,32 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const buttonEncode = document.querySelectorAll('#main > div > button')[0];
+    const buttonDecode = document.querySelectorAll('#main > div > button')[1];
+
+
+    buttonEncode.addEventListener('click', encode)
+    buttonDecode.addEventListener('click', decode)
+
+    function encode() {
+        const message = document.querySelectorAll('#main > div > textarea')[0].value;
+        const encodeMsg = document.querySelectorAll('#main > div > textarea')[1];
+
+        let result = '';
+        for (let char of message) {
+            asciiNum = char.charCodeAt(0) + 1;
+            result += String.fromCharCode(asciiNum);
+        }
+        encodeMsg.value = result;
+        document.querySelectorAll('#main > div > textarea')[0].value = '';
+    }
+
+    function decode() {
+        debugger
+        const messageDecode = document.querySelectorAll('#main > div > textarea')[1].value;
+        let result = '';
+        for (let char of messageDecode) {
+            asciiNum = char.charCodeAt(0) - 1;
+            result += String.fromCharCode(asciiNum);
+        }
+        document.querySelectorAll('#main > div > textarea')[1].value = result;
+    }
 }
