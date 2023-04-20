2d9363b35cb70b7d100f4ac3ae9794bbde1da4ae
ostrev <npostrev@gmail.com>
2023-04-20 08:12:46 +0300

    add

--- JS Front-End/15_Regular_Exam/Problem 02/Sprint-planning-solve/app.js
+++ JS Front-End/15_Regular_Exam/Problem 02/Sprint-planning-solve/app.js
@@ -1,165 +1,176 @@
 window.addEventListener('load', solve);
 
 function solve() {
-    inputDomSelectors = {
-        title: document.getElementById('title'),
-        description: document.getElementById('description'),
-        label: document.getElementById('label'),
-        points: document.getElementById('points'),
-        assignee: document.getElementById('assignee'),
+    const title = document.getElementById("title");
+    const description = document.getElementById("description");
+    const lebel = document.getElementById('label');
+   const estimationPoints = document.getElementById("points");
+    const assignee = document.getElementById("assignee");
+    const taskIDinput = document.getElementById("task-id");
 
-    }
+    const totalSprintPoints = document.getElementById("total-sprint-points");
+    const taskSection = document.getElementById("tasks-section")
 
-    otherDomSelectors = {
-        formRef: document.getElementById('create-task-form'),
-        taskIdHidden: document.getElementById('task-id'),
-        createTaskBtn: document.getElementById('create-task-btn'),
-        deleteTaskBtn: document.getElementById('delete-task-btn'),
-        tasksSection: document.getElementById('tasks-section'),
-        totalSprintPoints: document.getElementById('total-sprint-points')
-    }
+    const createBtn = document.getElementById("create-task-btn");
+    const deleteBtn = document.getElementById("delete-task-btn");
 
-    otherDomSelectors.createTaskBtn.addEventListener('click', createHandler);
-    otherDomSelectors.deleteTaskBtn.addEventListener('click', deleteHandler);
+    let totalNumberOfTasks = 0;
+    let totalPoints = 0;
+    let allTasks = {};
+    totalSprintPoints.innerText = `Total Points ${totalPoints}pts`;
 
-    let container = {}
-    let counter = 0
-    let priority = null
-    let icon = null
-    let taskId = null
-    let totalPoints = 0
+    createBtn.addEventListener ("click", addEventHandler);
+    deleteBtn.addEventListener("click", deleteTaskHandler)
 
-
-    function createHandler(event) {
+    function addEventHandler (event) {
         if (event) {
             event.preventDefault();
-        }    
+        }
+
+    let titleValue = title.value;
+    let descriptionValue = description.value;
+    let lebelValue = lebel.value;
+    let estimationPointsValue = estimationPoints.value
+    let assigneeValue = assignee.value;
+
+    if (titleValue === '' || descriptionValue === '' || 
+    lebelValue === '' || estimationPointsValue === '' || assigneeValue === '') {
+    return;
+    } 
+    totalNumberOfTasks += 1;
+    let taskId = `task-${totalNumberOfTasks}`;
+    allTasks[taskId] = {titleValue, descriptionValue, lebelValue, estimationPointsValue, assigneeValue};
+
+    const article = createElement ("article", taskSection, null, ["task-card"], taskId);
+    const divTask = createElement("div", article, "", ["task-card-label"])
     
-        let areEmpty = Object.values(inputDomSelectors)
-            .every((input) => input.value !== '');
+    if (lebelValue === "Feature") {
+    divTask.innerHTML = `Feature &#8865`;
+    divTask.classList.add('feature');
+    }
+    if (lebelValue === "Low Priority Bug") {
+        divTask.innerHTML = `Low Priority Bug &#9737`
+        divTask.classList.add('low-priority');
+    }
+    if (lebelValue === "High Priority Bug") {
+        divTask.innerHTML = `High Priority Bug &#9888`
+        divTask.classList.add('high-priority');
+    }
 
-        if (!areEmpty) {
-            console.log("Empty input");
-            return;
-        }
-        counter += 1
+    createElement("h3", article, titleValue, ["task-card-title"]);
+    createElement("p", article, descriptionValue, ["task-card-description"]);
+    createElement("div", article, `Estimated at ${estimationPointsValue} pts`, ["task-card-points"]);
+    createElement("div", article, `Assigned to: ${assigneeValue}`, ['task-card-assignee']);
+    const divDeleteBtn = createElement("div", article, null, ["task-card-actions"])
+    const deleteBtn2 = createElement("button", divDeleteBtn, "Delete")
 
-        container[`task-${counter}`] = {
-            title: inputDomSelectors.title.value,
-            description: inputDomSelectors.description.value,
-            label: inputDomSelectors.label.value,
-            points: inputDomSelectors.points.value,
-            assignee: inputDomSelectors.assignee.value
-        }
+    deleteBtn2.addEventListener("click", moveTaskHandler);
+
+    title.value = '';
+    description.value = '';
+    lebel.value = '';
+    estimationPoints.value = '';
+    assignee.value = '';
+
+    totalPoints += Number (estimationPointsValue);
+   totalSprintPoints.innerText = `Total Points ${totalPoints}pts`;
+
+    }
+
+    function moveTaskHandler() {
+    const articleId = this.parentNode.parentNode.id;
+
+    currentTask = allTasks[articleId];
+
+    title.value = currentTask['titleValue'];
+    description.value = currentTask['descriptionValue'];
+    lebel.value = currentTask["lebelValue"];
+    estimationPoints.value = currentTask["estimationPointsValue"];
+    assignee.value = currentTask["assigneeValue"];
+
+    title.disabled = true;
+    description.disabled = true;
+    lebel.disabled = true;
+    estimationPoints.disabled = true;
+    assignee.disabled = true;
+
+    taskIDinput.value = articleId.split('-')[1];
+    
+    createBtn.disabled = true;
+    deleteBtn.removeAttribute("disabled");
+   
+    }
+
+    function deleteTaskHandler (event) {
+if (event) {
+    event.preventDefault();
+}
+
+        let Id = "task-" + taskIDinput.value;
         
-        checkPriority(inputDomSelectors.label.value);
-
-        let article = createElement('article', null, otherDomSelectors.tasksSection, `task-${counter}`, ['task-card'] ); 
-        createElement('div', `${inputDomSelectors.label.value} ${icon}`, article, null, ['task-card-label', `${priority}`]);
-        createElement('h3', `${inputDomSelectors.title.value}`, article, null, ['task-card-title']);
-        createElement('p', `${inputDomSelectors.description.value}`, article, null, ['task-card-description']);
-        createElement('div', `Estimated at ${inputDomSelectors.points.value} pts`, article, null, ['task-card-points']);
-        createElement('div', `Assigned to: ${inputDomSelectors.assignee.value}`, article, null, ['task-card-assignee']);
-        let divBtn = createElement('div', null, article, null, ['task-card-actions']);
-        let deleteTaskCardBtn = createElement('button', 'Delete', divBtn)
-
-        deleteTaskCardBtn.addEventListener('click', deleteTaskCardHandler)
-
-        totalPoints += Number(inputDomSelectors.points.value)
-        otherDomSelectors.totalSprintPoints.textContent = `Total Points ${totalPoints}pts`
-
-        otherDomSelectors.formRef.reset()
-    }
-
-    function deleteHandler (event) {
-        if (event) {
-            event.preventDefault();
-        } 
-
-        let task = document.getElementById(`${taskId}`)
-        task.remove()
-
-        for (const input in inputDomSelectors) {
-            inputDomSelectors[input].removeAttribute('disabled')
-        }
-
-        otherDomSelectors.deleteTaskBtn.setAttribute('disabled', true);
-        otherDomSelectors.createTaskBtn.removeAttribute('disabled');
-
-        totalPoints -= Number(inputDomSelectors.points.value)
-        otherDomSelectors.totalSprintPoints.textContent = `Total Points ${totalPoints}pts`
-
-        otherDomSelectors.formRef.reset();
-    }
-
-    function deleteTaskCardHandler(event) {
-        if (event) {
-            event.preventDefault();
-        }
-
-        taskId = event.currentTarget.parentNode.parentNode.id
-        otherDomSelectors.taskIdHidden.value = taskId
-
-        for (const id in container) {
-            if (id === taskId) {
-                for (const key in container[id])
-                inputDomSelectors[key].value = container[id][key];
-            }
-        }
-        otherDomSelectors.deleteTaskBtn.removeAttribute('disabled');
-        otherDomSelectors.createTaskBtn.setAttribute('disabled', true);
         
-        for (const input in inputDomSelectors) {
-            inputDomSelectors[input].setAttribute('disabled', true)
-        }
+        totalPoints -= Number (estimationPoints.value);
+        
+        totalSprintPoints.innerText = `Total Points ${totalPoints}pts`;
+
+        const articleToDelete = document.getElementById(Id);
+        
+        articleToDelete.remove();
+        title.removeAttribute('disabled');
+        description.removeAttribute('disabled');
+        lebel.removeAttribute('disabled');
+        estimationPoints.removeAttribute('disabled');
+        assignee.removeAttribute('disabled');
+        title.value = '';
+        description.value = '';
+        lebel.value = '';
+        estimationPoints.value = '';
+        assignee.value = '';
+        taskIDinput.value = '';
+
+        createBtn.removeAttribute('disabled');
+        deleteBtn.setAttribute("disabled", true);
+
+
     }
 
-    function checkPriority( label ) {
-        if (label === 'Feature') {
-            priority = 'feature'
-            icon = '⊡'
-        } else if (label === 'Low Priority Bug') {
-            priority = 'low-priority';
-            icon = '☉'
-        } else if (label === 'High Priority Bug') {
-            priority = 'high-priority';
-            icon = '⚠'
-        }
 
-    }
-    function createElement(type, content, parentNode, id, classes, attributes, useInnerHtml) {
+
+
+    
+    function createElement(type, parentNode, content, classes, id, attributes, useInnerHtml) {
+    
         const htmlElement = document.createElement(type);
-    
         if (content && useInnerHtml) {
-            htmlElement.innerHTML = content;
-        } else {
-            if (content && type !== 'input') {
-                htmlElement.textContent = content;
-            }
-            if (content && type === 'input') {
-                htmlElement.value = content;
-            }
+          htmlElement.innerHTML = content;
+        } else  { 
+        if (content && type !== "input") {
+         htmlElement.textContent = content;
+        } 
+        if (content && type === 'input' ) {
+          htmlElement.value = content;
         }
+      }
     
-        if (parentNode) {
-            parentNode.appendChild(htmlElement);
-        }
+      if (classes && classes.length > 0) {
+        htmlElement.classList.add(... classes)
+      }
+      if (id) {
+        htmlElement.id = id;
+      }
     
-        if (id) {
-            htmlElement.id = id;
+      // src: 'link', href: 'http'
+      if (attributes) {
+        for (const key in attributes) {
+          htmlElement.setAttribute(key, attributes[key])
         }
-        // ['list', 'item']
-        if (classes) {
-            htmlElement.classList.add(...classes);
-        }
-        // {src: 'link to image', href: 'link to site', type: 'checkbox' }
-        if (attributes) {
-            for (const key in attributes) {
-                htmlElement.setAttribute(key, attributes[key])
-            }
-        }
-    
+      }
+       if (parentNode) {
+        parentNode.appendChild(htmlElement);
+       }
+      
+        
         return htmlElement;
-    
-    }
+      }
+
 }
